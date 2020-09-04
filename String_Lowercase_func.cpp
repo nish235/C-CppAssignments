e1e1bd62db2405b2be0bd656da15faab4147dcb4
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-04 14:19:38 +0530

    String Lowercase Using strlwr

---
+++ String_Lowercase_func.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char st[10];
+    cout<<"\n Enter String : ";
+    cin>>st;
+    strlwr(st);
+    cout<<"\n Lowercase String Is : "<<st<<endl;
+    return 0;
+}
