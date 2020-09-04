08d1125421f5224fe58b0ff73014d4f83a78e0cd
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-04 17:09:31 +0530

    String Reverse Without Using Function

---
+++ String_Reverse__Without_Func.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main() {
+  	char st[10];
+  	int i,len=0;
+  	cout<<"\n Enter the String : ";
+  	cin>>st;
+	for(i=0;st[i]!=0;i++)
+	{
+	    len++;
+	}
+  	cout<<"\n Reverse String Is : ";
+  	for(;len>=0;len--)
+    {
+        cout<<st[len];
+    }
+  	return 0;
+}
