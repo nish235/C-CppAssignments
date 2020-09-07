8f0933d4c4e93c93cab16bca295c933ab81dbc2d
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-07 12:43:41 +0530

    reads an integer and check the specified range
    Write a C program that reads an integer and check the specified range where it belongs. Print an error message if the number is negative and greater than 80

---
+++ Assign011.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"\n Enter Number : ";
+	cin>>num;
+	if(num>=0 && num<=80)
+	{
+		cout<<"\n NUMBER IS IN RANGE ";
+	}
+	else
+	{
+        cout<<"\n NOT IN RANGE ";
+	}
+	cout<<"\n\n\n\n";
+	return 0;
+}
