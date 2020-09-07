c96b045a058a12b83d29e639e80c523fcfe69796
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-07 12:46:19 +0530

    sum of all odd values
    Write a C program that read 5 numbers and sum of all odd values between them

---
+++ Assign012.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int num[5],i,tot=0;
+    cout<<"\n Enter First Number  : ";
+    cin>>num[0];
+    cout<<"\n Enter Second Number : ";
+    cin>>num[1];
+    cout<<"\n Enter Third Number  : ";
+    cin>>num[2];
+    cout<<"\n Enter Fourth Number : ";
+    cin>>num[3];
+    cout<<"\n Enter Fifth Number  : ";
+    cin>>num[4];
+    for(i=0;i<=5;i++)
+    {
+        if((num[i]%2)!=0)
+        {
+            tot=tot+num[i];
+        }
+    }
+    cout<<"\n\n\n Sum Of Odd Numbers Is : "<<tot;
+    cout<<"\n\n\n\n";
+    return 0;
+}
