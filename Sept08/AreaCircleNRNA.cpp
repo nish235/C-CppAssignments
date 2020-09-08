a0a6eb6d7d9668c4d977618c5c27bc87a7669c3a
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-08 13:57:17 +0530

    Area Of Circle NRNA
    Area Of Circle With No Return Type And No Arguements

---
+++ Sept08/AreaCircleNRNA.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void area()
+{
+    int rad;
+    float area;
+    cout<<"\n Enter Radius : ";
+    cin>>rad;
+    area=3.14f*rad*rad;
+    cout<<"\n Area = "<<area;
+    cout<<"\n\n";
+}
+int main()
+{
+    area();
+    return 0;
+}
