eef3adffa9557b35dd9ed726dd5fd2f6cb37f13a
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-10 15:13:42 +0530

    Shift Data by 2 bits to the Left

---
+++ Sept10/LeftShift.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cout<<"\n ENTER NUMBER : ";
+    cin>>n1;
+    cout<<"\n VALUE ENTERED IS : "<<n1<<endl;
+    n1=n1<<2;
+    n2=n1;
+    cout<<"\n THE SHIFTED DATA BY 2 BITS IS : "<<n2;
+    cout<<"\n\n";
+    return 0;
+}
