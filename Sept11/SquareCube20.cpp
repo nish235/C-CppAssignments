f8d175a643fac956ca9ad3ade9985b82967dc196
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-11 17:09:04 +0530

    squares and cubes of the numbers from 0 to 20

---
+++ Sept11/SquareCube20.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"\n Number\tSquare\tCube\n";
+    for(i=0;i<=20;i++)
+    {
+        cout<<" "<<i<<"\t"<<i*i<<"\t"<<i*i*i<<endl;
+    }
+    cout<<"\n\n";
+    return 0;
+}
