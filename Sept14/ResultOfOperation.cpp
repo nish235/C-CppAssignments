3b44489e2f7066e9d40d60a05e64eb790d1fd181
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-14 14:00:21 +0530

    Result Of Mathematical Operations

---
+++ Sept14/ResultOfOperation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double a,b,c,d;
+    a=-1+4*6;
+    b=(35+5)%7;
+    c=14+(-4)*6/11;
+    d=2+15/6*1-7%2;
+    cout<<"\n First Operation Results In  : "<<a;
+    cout<<"\n Second Operation Results In : "<<b;
+    cout<<"\n Third Operation Results In  : "<<c;
+    cout<<"\n Fourth Operation Results In : "<<d;
+    cout<<"\n\n";
+    return 0;
+}
