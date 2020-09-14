3cb2d4aeeaf9ba82fcd6973d490f711cce2c7bf4
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-14 14:01:24 +0530

    Multiplication Of 3 Numbers entered by User

---
+++ Sept14/MultiplicationOf3Num.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,res;
+    cout<<"\n Enter 1st Number : ";
+    cin>>a;
+    cout<<"\n Enter 2nd Number : ";
+    cin>>b;
+    cout<<"\n Enter 3rd Number : ";
+    cin>>c;
+    res=a*b*c;
+    cout<<"\n Multiplication   : "<<res;
+    cout<<"\n\n";
+    return 0;
+}
