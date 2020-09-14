00a1ae0b284981223413118d0752c493d874413a
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-14 15:50:47 +0530

    Temperature Conversion from Celsius to Fahrenheit

---
+++ Sept14/Temperature.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float c,f;
+    cout<<"\n Enter Temperature in Celsius : ";
+    cin>>c;
+    f=(c*9/5) + 32;
+    cout<<"\n Celsius to Fahrenheit : "<<f;
+    cout<<"\n\n";
+    return 0;
+}
