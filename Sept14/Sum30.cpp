f77a6ed83cc198749b397f0554cfb93d22dc3cb9
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-14 15:34:23 +0530

    True if one of them is 30 or if their sum is 30

---
+++ Sept14/Sum30.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"\n Enter First Number  : ";
+    cin>>a;
+    cout<<"\n Enter Second Number : ";
+    cin>>b;
+    c=a+b;
+    if(a==30 || b==30 || c==30)
+    {
+        cout<<"\n TRUE ";
+    }
+    else
+    {
+        cout<<"\n FALSE ";
+    }
+    cout<<"\n\n";
+    return 0;
+}
