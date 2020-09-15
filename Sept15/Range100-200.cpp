e9bc00b1360ab1ee86c61f953baea1e23c103826
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-15 14:35:20 +0530

    2 Integers in the range 100-200

---
+++ Sept15/Range100-200.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"\n Enter 1st Number : ";
+    cin>>a;
+    cout<<"\n Enter 2nd Number : ";
+    cin>>b;
+    if(a >= 100 && b <= 100) || (b >= 100 && b <= 200))
+    {
+        cout<<"\n TRUE ";
+    }
+    else
+    {
+        cout<<"\n FALSE ";
+    }
+    return 0;
+}
