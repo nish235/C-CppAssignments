f8fe195f3a3e0c72981792bfa7935d1b99b87a68
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-17 17:26:32 +0530

    check whether a given array of integers of length 2, contains 15 or 20

---
+++ Sept17/Check15or20.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,a[2];
+    cout<<"\n Enter Array : \n";
+    for(i=0;i<=1;i++)
+    {
+        cin>>a[i];
+    }
+    if(a[0]==15 || a[0]==20 ||a[1]==15 || a[1]==20)
+    {
+        cout<<"\n TRUE ";
+    }
+    else
+    {
+        cout<<"\n FALSE ";
+    }
+    cout<<"\n\n";
+    return 0;
+}
