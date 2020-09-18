12f65d4432c03707be95823d260f0a057efd0584
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-18 14:35:19 +0530

    Program to check if the sum of all 5' in the array exactly 15 in a given array of integers

---
+++ Sept18/03SumOf5sInArray.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int check(int num[], int arr)
+{
+    int sum=0;
+    for(int i=0;i<arr;i++)
+    {
+        if(num[i]==5)
+        {
+            sum=sum+5;
+        }
+    }
+    return sum == 15;
+}
+
+int main()
+{
+    int arr;
+    int arr1[]={1, 5, 6, 9, 10, 17};
+    arr=sizeof(arr1)/sizeof(arr1[0]);
+    cout<<"\n"<<check(arr1,arr);
+    int arr2[]={1, 5, 5, 5, 10, 17 };
+    arr=sizeof(arr2)/sizeof(arr2[0]);
+    cout<<"\n"<<check(arr2,arr);
+    int arr3[]={1, 1, 5, 5, 5, 5};
+    arr=sizeof(arr3)/sizeof(arr3[0]);
+    cout<<"\n"<<check(arr3,arr);
+    cout<<"\n\n";
+    return 0;
+}
+
