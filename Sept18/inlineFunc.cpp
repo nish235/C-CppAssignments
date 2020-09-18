abaa91e2106562467a6ca89759d017011002d2a4
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-18 11:59:54 +0530

    Example of Inline Function

---
+++ Sept18/inlineFunc.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+inline int multi(int a,int b)
+{
+    return a*b;
+}
+
+int main()
+{
+    int a,b;
+    cout<<"\n Enter a : ";
+    cin>>a;
+    cout<<"\n Enter b : ";
+    cin>>b;
+    cout<<"\n Multiplication is : "<<multi(a,b);
+    cout<<"\n Multiplication is : "<<multi(a,b);
+    cout<<"\n Multiplication is : "<<multi(a,b);
+    cout<<"\n Multiplication is : "<<multi(a,b);
+    cout<<"\n Multiplication is : "<<multi(a,b);
+    cout<<"\n Multiplication is : "<<multi(a,b);
+    cout<<"\n Multiplication is : "<<multi(a,b);
+    cout<<"\n Multiplication is : "<<multi(a,b);
+    cout<<"\n Multiplication is : "<<multi(a,b);
+    cout<<"\n\n";
+    return 0;
+}
