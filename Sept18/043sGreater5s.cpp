da9feb4fbf45536b46d7e8b4bd72a10f0ca9d8e6
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-18 15:01:18 +0530

    Program to check if the number of 3's is greater than the number of 5's

---
+++ Sept18/043sGreater5s.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int check(int num[], int arr)
+{
+    int n3=0,n5=0;
+    for(int i=0;i<arr;i++)
+    {
+        if(num[i]== 3)
+        {
+            n3++;
+        }
+        if(num[i]==5)
+        {
+            n5++;
+        }
+    }
+    return n3>n5;
+}
+
+int main()
+{
+    int arr;
+    int arr1[]={1, 5, 6, 9, 3, 3};
+    arr=sizeof(arr1)/sizeof(arr1[0]);
+    cout<<"\n"<<check(arr1,arr);
+    int arr2[]={1, 5, 5, 5, 10, 17};
+    arr=sizeof(arr2)/sizeof(arr2[0]);
+    cout<<"\n"<<check(arr2,arr);
+    int arr3[]={1, 3, 3, 5, 5, 5};
+    arr=sizeof(arr3)/sizeof(arr3[0]);
+    cout<<"\n"<<check(arr3,arr);
+    cout<<"\n\n";
+    return 0;
+}
