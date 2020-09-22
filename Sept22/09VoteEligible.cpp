d060e27930f91dc6a989cb1435d7d4048f725be3
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-22 15:59:30 +0530

    program to check if the person is eligible for vote or not

---
+++ Sept22/09VoteEligible.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int yr;
+   cout<<"\n Enter The Age : ";
+   cin>>yr;
+   if(yr>=18)
+   {
+       cout<<"\n YOU ARE ELIGIBLE TO VOTE ";
+   }
+   else
+   {
+       cout<<"\n YOU ARE NOT ELIGIBLE TO VOTE ";
+   }
+   cout<<"\n\n";
+   return 0;
+}
