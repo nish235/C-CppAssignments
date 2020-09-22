632a624aa71e690c9b37ce50fa3f2549d1cd7300
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-22 13:59:03 +0530

    program that takes three numbers (x,y,z) and print output (x+y).z and x.y+y.z

---
+++ Sept22/04xyz.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cout<<"\n Enter x : ";
+    cin>>x;
+    cout<<"\n Enter y : ";
+    cin>>y;
+    cout<<"\n Enter z : ";
+    cin>>z;
+    cout<<"\n (x+y).z = "<<(x+y)*z;
+    cout<<"\n\n x.y+y.z = "<<x*y+y*z;
+    cout<<"\n\n";
+    return 0;
+}
