0be72b62d27f76a7f786307eef7d284a83ff00d6
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-22 14:09:33 +0530

    Program that takes an age as input and print as "You look Older than 20"

---
+++ Sept22/05OlderThan20.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"\n Enter x : ";
+    cin>>x;
+    if(x<=20)
+    {
+        cout<<"\n You Look Younger ";
+    }
+    else
+    {
+        cout<<"\n You Look Older than 20 ";
+    }
+    cout<<"\n\n";
+    return 0;
+}
