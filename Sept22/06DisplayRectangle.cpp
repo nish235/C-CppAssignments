2440b0f94b4694fbc031879727590f702b10dfbb
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-22 15:25:22 +0530

    program that takes a number as input and then displays a rectangle of 3 columns wide and 5 rows tall using that digit

---
+++ Sept22/06DisplayRectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"\n Enter x : ";
+    cin>>x;
+    cout<<"\n\t"<< x << x << x;
+    cout<<"\n\t"<< x <<" "<< x;
+    cout<<"\n\t"<< x <<" "<< x;
+    cout<<"\n\t"<< x <<" "<< x;
+    cout<<"\n\t"<< x << x << x;
+    cout<<"\n\n ";
+    return 0;
+}
