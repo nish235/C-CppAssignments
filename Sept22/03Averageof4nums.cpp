2dc44eeb323582004dabc4fd371556f446044ca4
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-22 13:51:01 +0530

    program that takes four numbers as input to calculate and print the average of the numbers

---
+++ Sept22/03Averageof4nums.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2,n3,n4;
+    float avg=0;
+    cout<<"\n Enter 1st Number : ";
+    cin>>n1;
+    cout<<"\n Enter 2nd Number : ";
+    cin>>n2;
+    cout<<"\n Enter 3rd Number : ";
+    cin>>n3;
+    cout<<"\n Enter 4th Number : ";
+    cin>>n4;
+    avg=(n1+n2+n3+n4)/4;
+    cout<<"\n AVERAGE : "<<avg;
+    cout<<"\n\n";
+    return 0;
+}
