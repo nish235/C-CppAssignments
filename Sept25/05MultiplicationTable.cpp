9a76f17c897395becf00ad6cacc21cc4fe7e3438
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-25 13:55:12 +0530

    display the multiplication table of a given integer

---
+++ Sept25/05MultiplicationTable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int i,n;
+   cout<<"\n Enter The Number : ";
+   cin>>n;
+
+   for(i=1;i<=10;i++)
+   {
+        cout<<"\n\t"<<n<<" * "<<i<<" = "<<n*i;
+   }
+   cout<<"\n\n";
+   return 0;
+}
