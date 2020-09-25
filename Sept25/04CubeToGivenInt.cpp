82a6142d021359c9912ebb1712d37de3e7cb7d81
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-09-25 13:43:18 +0530

    display the cube of the number upto a given integer

---
+++ Sept25/04CubeToGivenInt.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,n;
+    cout<<"\n Enter The Number : ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+	 cout<<"\n Number is : "<<i<<" and Cube is : "<<i*i*i;
+    }
+    cout<<"\n\n";
+    return 0;
+}
