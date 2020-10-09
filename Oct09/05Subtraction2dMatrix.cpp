ada1218d36047dfe8b6aa97ece8aabe52524cc29
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-10-09 15:02:17 +0530

    subtraction of two Matrices

---
+++ Oct09/05Subtraction2dMatrix.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[10][10],b[10][10],c[10][10],i,j,n;
+    cout<<"\n Enter The Number : ";
+    cin>>n;
+    cout<<"\n Enter Elements in the First Matrix : \n";
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cout<<"\n element - "<<i+1<<","<<j+1<<" : ";
+            cin>>a[i][j];
+        }
+    }
+
+    cout<<"\n Enter Elements in the Second Matrix : \n";
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cout<<"\n element - "<<i+1<<","<<j+1<<" : ";
+            cin>>b[i][j];
+        }
+    }
+    cout<<"\n The First matrix is : \n";
+    for(i=0;i<n;i++)
+    {
+        cout<<"\n";
+        for(j=0;j<n;j++)
+            cout<<"\t"<<a[i][j];
+    }
+
+    cout<<"\n The Second matrix is : \n";
+    for(i=0;i<n;i++)
+    {
+        cout<<"\n";
+        for(j=0;j<n;j++)
+            cout<<"\t"<<b[i][j];
+    }
+
+    for(i=0;i<n;i++)
+       for(j=0;j<n;j++)
+            c[i][j]=a[i][j]-b[i][j];
+    cout<<"\n The Addition of two matrix is : \n";
+    for(i=0;i<n;i++)
+    {
+        cout<<"\n";
+        for(j=0;j<n;j++)
+            cout<<"\t"<<c[i][j];
+    }
+    cout<<"\n\n";
+    return 0;
+}
